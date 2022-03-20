359bd0d61d1d6809e666044291f5f07c8002fb29
huamintas03 <dlswp113@gmail.com>
2022-03-21 00:05:02 +0900

    environ을 통한 민감 설정 외부화 적용, local에 대한 세팅 분리

--- ginza/settings/base.py
+++ ginza/settings/base.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^69hq2n&x+j)a$(^a7_p6*b(-zfvl&(2ap+4a9)ql7m#awy4+3'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -68,18 +62,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ginza.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
