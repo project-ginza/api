f130580e6b0e92e743b74c407d262eb2921a6e8f
leeky940926 <leeky940926@gmail.com>
2022-04-14 20:24:13 +0900

    Base Modeling

--- order/models.py
+++ order/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from common.models import BaseModel
 
-# Create your models here.
+# 사용자 상태 enum
+class OrderStatus(models.IntegerChoices):
+    SUCCESS = 0   #주문완료
+    ARRIVE = 1  #배송완료
+    REFUND = 2  #환불신청완료
+    SHIPPING = 3 #배송중
+
+class Order(BaseModel):
+    product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT)
+    user = models.ForeignKey("user.User", on_delete=models.PROTECT)
+    status = models.IntegerField(
+        choices=OrderStatus.choices,
+        default=OrderStatus.SUCCESS
+    )
+
