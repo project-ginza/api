070c926a684ad370f217e4e5e212cf9220374cc8
leeky940926 <leeky940926@gmail.com>
2022-04-15 20:12:30 +0900

    order modeling 1차 완료

--- order/models.py
+++ order/models.py
@@ -8,11 +8,18 @@ class OrderStatus(models.IntegerChoices):
     REFUND = 2  #환불신청완료
     SHIPPING = 3 #배송중
 
-class Order(BaseModel):
+class Basket(BaseModel):
     product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT)
     user = models.ForeignKey("user.User", on_delete=models.PROTECT)
+    count = models.PositiveIntegerField()
+
+class Order(BaseModel):
+    product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey("user.User", on_delete=models.PROTECT, null=True)
     status = models.IntegerField(
         choices=OrderStatus.choices,
         default=OrderStatus.SUCCESS
     )
+    basket = models.ForeignKey("order.Basket", on_delete=models.CASCADE, null=True)
+    count = models.PositiveIntegerField(null=True)
 
