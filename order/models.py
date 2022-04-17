920f8525abbba5e370ba318b71583eb8848da023
leeky940926 <leeky940926@gmail.com>
2022-04-17 17:22:07 +0900

    수량 컬럼명 변경 및 주문 상태 값 변경

--- order/models.py
+++ order/models.py
@@ -7,17 +7,17 @@ class OrderStatus(BaseModel):
     class Meta:
         db_table = 'order_status'
 '''
-SUCCESS = 0   #주문완료
-ARRIVE = 1  #배송완료
-REFUND = 2  #환불신청완료
-SHIPPING = 3 #배송중
+SUCCESS = 0  주문완료
+ARRIVE = 1   배송완료
+REFUND = 2   환불신청완료
+SHIPPING = 3  배송중
 
 '''        
 
 class Basket(BaseModel):
     product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT)
     user = models.ForeignKey("user.User", on_delete=models.PROTECT)
-    count = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
     
     class Meta:
         db_table = "basket"
@@ -27,7 +27,7 @@ class Order(BaseModel):
     user = models.ForeignKey("user.User", on_delete=models.PROTECT, null=True)
     status = models.ForeignKey("order.OrderStatus", on_delete=models.PROTECT)
     basket = models.ForeignKey("order.Basket", on_delete=models.CASCADE, null=True)
-    count = models.PositiveIntegerField(null=True)
+    quantity = models.PositiveIntegerField(null=True)
     
     class Meta:
         db_table = "order"
