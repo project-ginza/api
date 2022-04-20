4109405a8cb28be5413aacc6c6edfd987db690e4
leeky940926 <leeky940926@gmail.com>
2022-04-20 21:01:05 +0900

    주문상태 Enum으로 최종확정

--- order/models.py
+++ order/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from common.models import BaseModel
 
-class OrderStatus(BaseModel):
-    status = models.CharField()
-    
-    class Meta:
-        db_table = 'order_status'
 '''
-SUCCESS = 0  주문완료
-ARRIVE = 1   배송완료
-REFUND = 2   환불신청완료
-SHIPPING = 3  배송중
-
-'''        
+orderstatus enum으로 바꾸기
+'''
+class OrderStatus(models.IntegerChoices):    
+    SUCCESS = 0   #주문완료
+    ARRIVE = 1    #배송완료
+    REFUND = 2    #환불신청완료
+    SHIPPING = 3  #배송중
+       
 
 class Basket(BaseModel):
     product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT)
@@ -25,7 +22,7 @@ class Basket(BaseModel):
 class Order(BaseModel):
     product = models.ForeignKey("product.ProductDetails", on_delete=models.PROTECT, null=True)
     user = models.ForeignKey("user.User", on_delete=models.PROTECT, null=True)
-    status = models.ForeignKey("order.OrderStatus", on_delete=models.PROTECT)
+    status = models.IntegerField(choices=OrderStatus.choices, default=OrderStatus.SUCCESS)
     basket = models.ForeignKey("order.Basket", on_delete=models.CASCADE, null=True)
     quantity = models.PositiveIntegerField(null=True)
     
