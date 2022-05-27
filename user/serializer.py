f5faeda2eb1fb5af338de9b946a4dc45fbeb2858
Marshall Lee <marshall.s.lee@gmail.com>
2022-05-27 10:41:12 +0900

    swagger 문서 작업

--- user/serializer.py
+++ user/serializer.py
@@ -1,30 +1,18 @@
 from rest_framework import serializers
-from user.models import User, UserProfile
 
 
-class UserSerializer(serializers.ModelSerializer):
+class SignupRequestBodySerializer(serializers.Serializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
     email = serializers.EmailField()
     password = serializers.CharField()
     name = serializers.CharField()
-
-    class Meta:
-        model = User
-        fields = '__all__'
-        # fields = ('email, password, name',)
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
     mobile = serializers.CharField(max_length=11)
     birthday = serializers.DateField()
     is_solar_calendar = serializers.BooleanField()
     agreed_with_mkt_info_subscription = serializers.BooleanField()
 
-    class Meta:
-        model = UserProfile
-        fields = '__all__'
-        # fields = ('mobile', 'birthday', 'is_solar_calendar', 'agreed_with_mkt_info_subscription',)
-
-
-class SignupRequestBodySerializer(UserSerializer, UserProfileSerializer):
-    def __init__(self):
-        super(SignupRequestBodySerializer, self).__init__()
